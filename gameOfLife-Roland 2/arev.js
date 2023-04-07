f8f087dc6f0bf8828c4cda811da51d217f6b2063
RolandSargsyan <roland.sargsyan@tumo.org>
2023-04-07 20:19:26 +0400

    first commit

---
+++ gameOfLife-Roland 2/arev.js
@@ -0,0 +1,122 @@
+class Sun {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 100
+        this.directions = [];
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+
+            [this.x + 2, this.y - 1],
+            [this.x + 2, this.y],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y + 2],
+
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x - 2, this.y + 2],
+
+            [this.x - 2, this.y - 1],
+            [this.x - 2, this.y],
+            [this.x - 2, this.y + 1],
+
+        ];
+    }
+
+    chooseCell(char) {
+        this.getNewCoordinates();
+        let found = [];
+
+        for (let i in this.directions) {
+            let x = this.directions[i][0];
+            let y = this.directions[i][1];
+
+            if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+
+        }
+
+        return found;
+    }
+    
+    eat() {
+        let emptyCell = this.chooseCell(1);
+        let newCell = random(emptyCell)
+
+        if (newCell) {
+
+            let newX = newCell[0];
+            let newY = newCell[1];
+
+
+
+            for (let i = 0; i < grassArr.length; i++) {
+                if (grassArr[i].x == newX && grassArr[i].y == newY) {
+                    grassArr.splice(i, 1)
+                    break;
+                }
+            }
+
+            
+
+            matrix[newY][newX] = 5;
+            matrix[this.y][this.x] = 6;
+
+            this.x = newX;
+            this.y = newY;
+
+
+
+
+        }
+
+        
+
+
+        else {
+            this.move()
+        }
+    }
+
+    move() {
+        let emptyCell = this.chooseCell(0);
+        let newCell = random(emptyCell)
+
+        if (newCell) {
+            let newX = newCell[0];
+            let newY = newCell[1];
+
+            matrix[newY][newX] = 5;
+            matrix[this.y][this.x] = 0;
+
+
+            this.x = newX;
+            this.y = newY;
+
+
+        }
+    }
+
+
+    
+}
---
+++ gameOfLife-Roland 2/jur.js
@@ -0,0 +1,63 @@
+class Water {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 100
+        this.directions = [];
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(char) {
+        this.getNewCoordinates();
+        let found = [];
+
+        for (let i in this.directions) {
+            let x = this.directions[i][0];
+            let y = this.directions[i][1];
+
+            if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+
+        }
+
+        return found;
+    }
+
+
+    move() {
+        let emptyCell = this.chooseCell(0);
+        let newCell = random(emptyCell)
+
+        if (newCell) {
+            let newX = newCell[0];
+            let newY = newCell[1];
+
+            matrix[newY][newX] = 4;
+            matrix[this.y][this.x] = 1;
+
+
+            this.x = newX;
+            this.y = newY;
+
+            this.energy--
+
+            
+
+        }
+    }
+    
+ }
