f18825a3cd42324ddf62dea671a2fae148982723
RolandSargsyan <roland.sargsyan@tumo.org>
2023-04-09 19:34:32 +0400

    Js class inheritance

--- gameOfLife-Roland 2/arev.js
+++ gameOfLife-Roland 2/arev.js
@@ -1,8 +1,6 @@
-//cdwhgdayudgayudgay
-class Sun {
+class Sun extends LivingCreature{
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+     super(x,y)
         this.energy = 100
         this.directions = [];
     }
@@ -42,22 +40,8 @@ class Sun {
 
     chooseCell(char) {
         this.getNewCoordinates();
-        let found = [];
-
-        for (let i in this.directions) {
-            let x = this.directions[i][0];
-            let y = this.directions[i][1];
-
-            if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
-                if (matrix[y][x] == char) {
-                    found.push(this.directions[i]);
-                }
-            }
-
-
-        }
-
-        return found;
+        
+        return super.chooseCell(char);
     }
     
     eat() {
