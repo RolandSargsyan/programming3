cb778c92368a1b15cab818d62beafd2380b16b1d
RolandSargsyan <130167787+RolandSargsyan@users.noreply.github.com>
2023-04-21 20:22:54 +0400

    started server

---
+++ grass.js
@@ -0,0 +1,39 @@
+let LivingCreature = require("./LivingCreature")
+
+module.exports = class Grass extends LivingCreature {
+    constructor(x,y){
+            super(x,y)
+            this.multiply = 0
+            this.directions = [
+                [this.x - 1, this.y - 1],
+                [this.x    , this.y - 1],
+                [this.x + 1, this.y - 1],
+                [this.x - 1, this.y    ],
+                [this.x + 1, this.y    ],
+                [this.x - 1, this.y + 1],
+                [this.x    , this.y + 1],
+                [this.x + 1, this.y + 1]
+            ];
+        
+    }
+
+    
+
+    mul(){
+        this.multiply++
+        var emptyCell = this.chooseCell(0)
+        var newCell = emptyCell[Math.floor(Math.random() * emptyCell.length)]
+
+            if(this.multiply >= 8 && newCell){
+                  var newX  = newCell[0]
+                  var newY = newCell[1]
+
+                  matrix[newY][newX] = 1
+
+                  var gr  = new Grass(newX,newY)
+                  grassArr.push(gr)
+                  this.multiply = 0
+            }
+
+    }
+}
