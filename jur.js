123ba581d74ac37f019dc4f483a9e3c896071562
RolandSargsyan <130167787+RolandSargsyan@users.noreply.github.com>
2023-04-30 19:58:12 +0400

    final process

---
+++ jur.js
@@ -0,0 +1,49 @@
+let LivingCreature = require("./LivingCreature")
+
+module.exports = class Water extends LivingCreature{
+    constructor(x, y) {
+        super(x,y)
+        this.energy = 100
+        this.directions = [];
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(char) {
+        this.getNewCoordinates();
+        return super.chooseCell(char);
+    }
+
+
+    move() {
+        let emptyCell = this.chooseCell(0);
+        let newCell = emptyCell[Math.floor(Math.random() * emptyCell.length)]
+
+        if (newCell) {
+            let newX = newCell[0];
+            let newY = newCell[1];
+
+            matrix[newY][newX] = 4;
+            matrix[this.y][this.x] = 1;
+
+
+            this.x = newX;
+            this.y = newY;
+
+            this.energy--
+
+            
+
+        }
+    }
+    
+ }
